44c6d0d00ce36855d22978e98e1d763e6e37ff90
vatolkin <vatolkin@mail.ru>
2015-08-07 21:34:14 +0300

    чистка кода
    Сделал код чуть красивее

--- www/message.php
+++ www/message.php
@@ -1,13 +1,13 @@
 <?php
-/*тексты ошибок*/
 
+/* Тексты ошибок */
+$message['complete']               = 'Завершено';
+$message['login_found']            = 'Такой никнейм уже существует';
+$message['message_send']           = 'Сообщение отправлено';
+$message['message_empty']          = 'Пустое сообщение';
+$message['no_wrtite_to_db']        = 'Ошибка записи в базу';
+$message['not_min_message']        = 'Сообщение слишком короткое';
+$message['encorrected_query']      = 'Некорректный запрос';
 $message['login_encorretcted']     = 'Некорректный логин';
 $message['password_encorretcted']  = 'Некорректный пароль';
-$message['login_found']            = 'Такой никнейм уже существует';
 $message['user_type_encorretcted'] = 'Некорректный тип персонажа';
-$message['no_wrtite_to_db']        = 'Ошибка записи в базу';
-$message['complete']        = 'Завершено';
-$message['message_empty']        = 'Пустое сообщение';
-$message['not_min_message']        = 'Сообщение слишком короткое';
-$message['message_send']        = 'Сообщение отправлено';
-$message['encorrected_query']        = 'Некорректный запрос';
